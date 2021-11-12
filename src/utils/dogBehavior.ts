d128216393f8777b301edebd064c74c21f2a4329
Guilherme Andrade <gui-keanne@hotmail.com>
2021-11-12 10:09:20 -0300

    criando tabelas de apoio

---
+++ src/utils/dogBehavior.ts
@@ -0,0 +1,6 @@
+export const dogBehavior = [
+  { key: '1', behavior: 'Dócil' },
+  { key: '2', behavior: 'Companheiro' },
+  { key: '3', behavior: 'Agitado' },
+  { key: '4', behavior: 'Bravo' },
+];
---
+++ src/utils/dogVaccine.ts
@@ -0,0 +1,7 @@
+export const dogVaccine = [
+  { key: '1', vaccine: 'Antirrábica' },
+  { key: '2', vaccine: 'Polivalente' },
+  { key: '3', vaccine: 'Giardíase' },
+  { key: '4', vaccine: 'Gripe Canina' },
+  { key: '5', vaccine: 'Leishmaniose' },
+];
